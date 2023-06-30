b5c0b3637b320a9e9c4c0be000dce23c3a4696e1
AkmalMir <45818147+AkmalMir@users.noreply.github.com>
2023-06-30 17:43:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-# python -m streamlit run main.py
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 import shap
 import lightgbm as lgb
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
-#from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
 from lime.lime_tabular import LimeTabularExplainer
 import streamlit.components.v1 as components
-#import IPython
-#import pipreqs
 
-#if __name__ == "__main__":
-#    pipreqs.get_pip_requirements()
-#
+
+
 
 MODEL = lgb.LGBMRegressor() # #
 
@@ -36,11 +29,13 @@ with header:
 # Project Title: Melbourn House Price Prediction
 # Explanation
 st.cache_data()
+
 with dataset:
     st.header("House Prices in Melbourne")
     st.text("dataset in Kaggle.com")
     df = pd.read_csv('melb_data.csv')
     st.write(df.head())
+
 with features:
     st.subheader("Distribution of Number Rooms")
     price_dist = pd.DataFrame(df['Rooms'].value_counts())
@@ -55,6 +50,8 @@ with features:
                    ]
     X = numeric_cols[cols_select].astype(int)
 
+
+# fit model to dataset
 st.cache_resource()
 with model_training:
     model = MODEL 
@@ -62,7 +59,6 @@ with model_training:
 
 st.sidebar.header("Select Params")
 with model_prediction:
-#with predicting():
     def newfeatures():
         ROOMS = st.sidebar.slider('Rooms', int(X['Rooms'].min()), int(X['Rooms'].max())) #X['Rooms'].mean(), format='%d'
         BATHROOMS = st.sidebar.slider('Bathroom', int(X['Bathroom'].min()), int(X['Bathroom'].max())) #X['Bathroom'].mean(), format='%d'
@@ -96,30 +92,7 @@ with model_prediction:
     st.header("Predicted House Price")
     st.write(prediction)
     st.write("---")
-
-    #with explanation:
-    # Split the dataset into a training set and a test set
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,random_state=99)
-    #model = MODEL #RandomForestRegressor(random_state=42)
-    #model.fit(X_train,y_train)
-    #preds = model.predict(X_test)
-
-    #with explanation:
-    #st.header("Model Metrics")
-    #st.write("Mean absolute error of the model is: ")
-    #st.write(int(mean_absolute_error(y_test,preds)))
-    #st.write("Root_mean_squared_error of the model is: ")
-    #st.write(int(mean_squared_error(y_test,preds,squared=False)))
-    #st.write("r2_score of the model is: ")
-    #st.write(r2_score(y_test,preds).round(2)) 
-
-    #df_metric = pd.DataFrame({
-    #    "Metrics": ["MAE", "RMSE", "R2 Score"],
-    #    "Value": [int(mean_absolute_error(y_test, preds)), int(mean_squared_error(y_test, preds, squared=False)), r2_score(y_test, preds).round(2)]
-    #})
-    #st.write(df_metric)
-
-#st.write(df_metric)
+    
 with explanation:
     #plt.title("Feature importance based on SHAP Values")
     explainer = shap.TreeExplainer(model)
@@ -132,10 +105,7 @@ with explanation:
     st.pyplot(bbox_inches='tight')
     st.write("---")
 
-    # visualize the first prediction's explanation
-    # Define the feature names for the dataset
-
-    # LIMEEEEEEEEEEE
+    # LIME Prediction Exlanation
 
     st.header("Prediction Explanation")
     feature_names = X.columns
@@ -157,5 +127,4 @@ with explanation:
     #st.write(explanation.show_in_notebook(show_table=True),unsafe_allow_html=True)
 
     html = explanation.as_html()
-    components.html(html, height=800) #, 
-    #######################################################
+    components.html(html, height=800) 
